fd62e261182d8ea6d36eadd0ef678d30ee45f738
Nandun <mkntharaka@gmail.com>
2025-02-05 21:29:49 +0530

    Work in progress, fetched pet data

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ async function start() {
   document.querySelector('#temperature-output').textContent = ourTemperature
 }
 start()
+
+async function petsArea() {
+  const petsPromise = await fetch(
+    'https://learnwebcode.github.io/bootcamp-pet-data/pets.json'
+  )
+  const petsData = await petsPromise.json()
+  petsData.forEach(pet => {
+    console.log(pet.species)
+  })
+}
+
+petsArea()
